ca2cb9c9aa632ce2d4b60ef3c323b18a18b1c11a
Travis Nesland <travis@tnez.dev>
2024-04-17 13:04:53 -0400

    Clean up some issues on `examples/load_from_json`

--- examples/load_from_json/main.py
+++ examples/load_from_json/main.py
@@ -6,16 +6,6 @@ from scoutos import App
 
 
 async def main() -> None:
-    """
-    This is a simple example illustrating how to use the ScoutOS SDK to run a
-    simple application.
-
-    Parameters:
-    - app_input (AppInput): The input data required for the application to run.
-
-    Returns:
-    - None: This function prints the result of the application run to the console.
-    """
     path = Path("./app.json")
     app = App.load_from_file(path)
     result = await app.run()
