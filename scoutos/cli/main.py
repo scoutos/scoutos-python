0aaf2f42ea191745391f80b129652273ce8f6864
Travis Nesland <travis@tnez.dev>
2024-04-18 13:07:11 -0400

    Complete initial version of working CLI
    This version comes with two subcommands:
    
    - about: prints a message about the tool (really just using as a
             placeholder to simulate multiple commands)
    - run: run an app represented in a config file

--- scoutos/cli/main.py
+++ scoutos/cli/main.py
@@ -1,22 +1,42 @@
+from __future__ import annotations
+
+import asyncio
+from pathlib import Path
+from typing import Annotated
+
 import typer
+
+from scoutos import App
+from scoutos.cli.utils import parse_json
 
 app = typer.Typer()
 
 
-def create_greeting(name: str) -> str:
-    return f"Hello, {name}!"
+@app.command()
+def about() -> None:
+    message = """
+    This is the ScoutOS CLI designed to help interact with the ScoutOS platform.
+    """
+    typer.echo(message)
 
 
 @app.command()
-def greet(name: str = "You") -> None:
-    greeting = create_greeting(name)
-    print(greeting)
+def run(
+    path: Annotated[str, typer.Argument()],
+    app_input: Annotated[
+        str,
+        typer.Option(
+            ...,
+            help="JSON Stringified data to be used as app run input",
+        ),
+    ] = "",
+) -> None:
+    config_file_path = Path(path)
+    app = App.load_from_file(config_file_path)
+    parsed_app_input = parse_json(app_input)
+    result = asyncio.run(app.run(parsed_app_input))
 
-
-@app.command()
-def yell(name: str = "You") -> None:
-    greeting = create_greeting(name).upper()
-    print(greeting)
+    typer.echo(result)
 
 
 if __name__ == "__main__":
--- scripts/run_examples.sh
+++ scripts/run_examples.sh
@@ -7,12 +7,15 @@ ROOT_DIR=$(pwd)
 EXAMPLES_DIR="./examples"
 
 for example_dir in "$EXAMPLES_DIR"/*; do
-  if [ -d "$example_dir" ] && [ "$example_dir" != "./examples/load_from_yaml" ]; then
+  if [ -d "$example_dir" ]; then
+    echo ""
     echo "Running test for example: $example_dir"
+    echo "---"
     cd "$example_dir" || exit
     make setup
     make test
     make teardown
+    echo "OK"
     cd "$ROOT_DIR" || exit
   fi
 done
