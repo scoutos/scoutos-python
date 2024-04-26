967c076e69e6553807ee6ec76832847478907a18
Travis Nesland <travis@tnez.dev>
2024-04-26 12:22:22 -0400

    PROD-598: New Block `scoutos:slack:get_thread` (#15)
    ## Ticket
    
    
    https://linear.app/scoutshq/issue/PROD-598/block-slack-get-all-messages-for-a-given-thread-id
    
    ## What / Why?
    
    Create a new block `scoutos:slack:get_thread` to get all messages for a
    given thread.

---
+++ scoutos/blocks/slack/get_thread.py
@@ -0,0 +1,82 @@
+from __future__ import annotations
+
+from typing import Literal
+
+import httpx
+from pydantic import BaseModel, ValidationError
+
+from scoutos.blocks import BlockExecutionError
+
+from .base import Slack
+from .types import Message, ResponseMetadata  # noqa: TCH001
+
+
+class GetThreadInput(BaseModel):
+    channel: str
+    """Channel ID to which the thread belongs."""
+
+    ts: str
+    """Unique identifier of either a thread's parent message or a message in
+    the thread. ts must be the timestamp of an existing message with 0 or
+    more replies. If there are no replies then just the single message
+    referenced by ts will return - it is just an ordinary, unthreaded
+    message."""
+
+    cursor: str | None = None
+    """Paginate through collections of data by setting the cursor parameter to
+    a next_cursor attribute returned by a previous request's
+    response_metadata. Default value fetches the first "page" of the
+    collection. See pagination for more detail."""
+
+    latest: str | None = None
+    """Only messages before this Unix timestamp will be included in results.
+    Default is the current time."""
+
+    limit: int | None = None
+    """The maximum number of items to return. Fewer than the requested number of
+    items may be returned, even if the end of the conversation history
+    hasn't been reached. Maximum of 999."""
+
+    oldest: str | None = None
+    """Only messages after this Unix timestamp will be included in results.
+    Default is '0'.
+    """
+
+
+class GetThreadOutput(BaseModel):
+    ok: Literal[True]
+    messages: list[Message]
+    has_more: bool
+    response_metadata: ResponseMetadata | None = None
+
+
+class GetThread(Slack):
+    TYPE = "scoutos:slack:get_thread"
+
+    async def run(self, run_input: dict) -> GetThreadOutput:
+        validated_input = GetThreadInput.model_validate(run_input)
+
+        headers = self._headers
+        params = validated_input.model_dump()
+        url = f"{self._http_api_url}/conversations.replies"
+
+        async with httpx.AsyncClient() as client:
+            try:
+                response = await client.get(url, headers=headers, params=params)
+                response.raise_for_status()
+
+                data = response.json()
+
+                if not data.get("ok"):
+                    message = data.get("error", "an unknown exception occurred")
+                    raise BlockExecutionError(message)
+
+                return GetThreadOutput.model_validate(data)
+
+            except ValidationError as validation_error:
+                message = "output failed data validation"
+                raise BlockExecutionError(message) from validation_error
+
+            except httpx.HTTPStatusError as http_status_error:
+                message = "http request failed"
+                raise BlockExecutionError(message) from http_status_error
---
+++ tests/blocks/slack/test_get_thread.py
@@ -0,0 +1,110 @@
+from __future__ import annotations
+
+from unittest.mock import patch
+
+import httpx
+import pytest
+
+from scoutos.blocks.base import BlockExecutionError
+from scoutos.blocks.slack import GetThread
+
+FAKE_TOKEN = "xoxb-***"  # noqa: S105
+FAKE_CHANNEL_ID = "CXXX"
+FAKE_TS = "1712163070.080049"
+
+HTTPX_GET_PATCH_PATH = "scoutos.blocks.slack.get_thread.httpx.AsyncClient.get"
+
+
+def create_block():
+    return GetThread(
+        {
+            "key": "slack_get_thread",
+            "token": FAKE_TOKEN,
+        }
+    )
+
+
+def create_stubbed_response(
+    *,
+    status: int = 200,
+    json: dict | None = None,
+    text: str | None = None,
+):
+    if text:
+        response = httpx.Response(status, text=text)
+    else:
+        response = httpx.Response(status, json=json)
+
+    response._request = httpx.Request(  # noqa: SLF001
+        "GET", "https://slack.com/api/conversations.replies"
+    )
+
+    return response
+
+
+@pytest.mark.asyncio()
+async def test_happy_path(get_fixture_data):
+    block = create_block()
+    stubbed_response = create_stubbed_response(
+        json=get_fixture_data("tests/blocks/slack/fixtures/get_thread_payload.json")
+    )
+
+    with patch(
+        HTTPX_GET_PATCH_PATH, return_value=stubbed_response
+    ) as mock_http_request:
+        result = await block.run({"channel": FAKE_CHANNEL_ID, "ts": FAKE_TS})
+
+        mock_http_request.assert_called_once_with(
+            "https://slack.com/api/conversations.replies",
+            headers={"Authorization": f"Bearer {FAKE_TOKEN}"},
+            params={
+                "channel": FAKE_CHANNEL_ID,
+                "ts": FAKE_TS,
+                "cursor": None,
+                "latest": None,
+                "limit": None,
+                "oldest": None,
+            },
+        )
+
+        assert result is not None
+
+
+#
+#
+@pytest.mark.asyncio()
+async def test_when_slack_api_returns_error():
+    block = create_block()
+    stubbed_response = create_stubbed_response(
+        json={"ok": False, "error": "some_error"}
+    )
+
+    with patch(
+        HTTPX_GET_PATCH_PATH,
+        return_value=stubbed_response,
+    ), pytest.raises(BlockExecutionError, match="some_error"):
+        await block.run({"channel": FAKE_CHANNEL_ID, "ts": FAKE_TS})
+
+
+@pytest.mark.asyncio()
+async def test_when_slack_api_returns_invalid_data():
+    block = create_block()
+    stubbed_response = create_stubbed_response(json={"ok": True})
+
+    with patch(
+        HTTPX_GET_PATCH_PATH,
+        return_value=stubbed_response,
+    ), pytest.raises(BlockExecutionError, match="data validation"):
+        await block.run({"channel": FAKE_CHANNEL_ID, "ts": FAKE_TS})
+
+
+@pytest.mark.asyncio()
+async def test_when_reqeust_fails():
+    block = create_block()
+    stubbed_response = create_stubbed_response(status=500, text="Internal Server Error")
+
+    with patch(
+        HTTPX_GET_PATCH_PATH,
+        return_value=stubbed_response,
+    ), pytest.raises(BlockExecutionError, match="http request failed"):
+        await block.run({"channel": FAKE_CHANNEL_ID, "ts": FAKE_TS})
